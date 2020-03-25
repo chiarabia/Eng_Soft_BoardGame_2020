e6be6c652db7fe4244b967ec6fa2a01f806ac3c2
vittoriodani <10614148@polimi.it>
2020-03-25 23:20:05 +0100

    New Effects & Consolidate classes added

---
+++ src/main/java/it/polimi/ingsw/Effects/UnderMyself.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw.Effects;
+
+import it.polimi.ingsw.Board;
+import it.polimi.ingsw.Cell;
+import it.polimi.ingsw.Turn;
+import java.util.Set;
+
+public class UnderMyself extends StandardBuild {
+    public Set<Cell> build(Cell workerCell, Board board, Turn turn) {
+        Set <Cell> standardBuild = super.build(workerCell, board, turn);
+        if (workerCell.getZ()<3) standardBuild.add(workerCell);
+        return standardBuild;
+    }
+}
