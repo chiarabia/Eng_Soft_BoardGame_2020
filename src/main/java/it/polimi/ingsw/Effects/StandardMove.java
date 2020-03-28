ae6e26a0f5ed7070c5fe004b6ccf97a4aea2c5c2
Chiara Bianchimani <36208248+chiara011@users.noreply.github.com>
2020-03-28 12:16:31 +0100

    Added StandardLoseCoondition and javadoc for StandardMove (#10)
    * Added Standard Lose Condition class
    
    * Fixed mistake in Standard Move and added first draft of Javadoc
    
    * fixed indentation
    
    * Revert "fixed indentation"
    
    This reverts commit a8222e366be6afcb7329d922edd68eb4883605bb.
    
    * Revert "Fixed mistake in Standard Move and added first draft of Javadoc"
    
    This reverts commit 9bdba25ecd7f5ca218fb8620ec7c7efc1a192c21.
    
    * Revert "Revert "Fixed mistake in Standard Move and added first draft of Javadoc""
    
    This reverts commit a202594dc0370f5b1377aee0c984694f999d0524.
    
    * Added javadoc to StandardMove

--- src/main/java/it/polimi/ingsw/Effects/StandardMove.java
+++ src/main/java/it/polimi/ingsw/Effects/StandardMove.java
@@ -7,29 +7,43 @@ import it.polimi.ingsw.Turn;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-//Questo è il metodo principale per sapere quali sono le caselle attorno ad un lavoratore disponibili per uno spostamento
+/**
+ * This classe defines the general rules of movement
+ */
 
 public class StandardMove {
+
+    /**
+     * The general move method
+     * @param workerCell the worker's Cell
+     * @param board the board
+     * @param turn the player's turn
+     * @return a Set<Cell> collect that only has the cells where the player can move to
+     */
     public Set<Cell> move (Cell workerCell, Board board, Turn turn) {
         final Set<Cell> collect = board.getStream()
-                /*(Questo metodo serve poichè l'effettiva rappresentazione della tabella é private e non deve
-                essere mai esposta, con questo metodo chiedo alla Board di darmi uno stream della sua rappresentazione. Lo
-                 stream è una copia degli elementi della rappresentazione interna, e (se ben ricordo) non dovrebbe modificare nulla
-                 nella Board)*/
-                .filter(Cell::isFree) //filtro le caselle libere
+                //only free Cells
+                .filter(Cell::isFree)
+                //only the Cells near the worker
                 .filter(a -> a.getX() <= workerCell.getX() + 1)
                 .filter(a -> a.getX() >= workerCell.getX() - 1)
                 .filter(a -> a.getY() <= workerCell.getY() + 1)
-                .filter(a -> a.getY() >= workerCell.getY() - 1) //Filtro tutte le caselle attorno, al massimo le 8 colonne
-                //attorno al mio lavoratore
-                .filter(a -> heightsDifference(workerCell.getZ(), a.getZ()) <= 1) //riduco le caselle in cui posso spostarmi,
-                //devono essere raggiungibili in altezza
+                .filter(a -> a.getY() >= workerCell.getY() - 1)
+                //the worker can move up only by one level, but can move down without limitations
+                .filter(a -> heightsDifference(workerCell.getZ(), a.getZ()) <= 1)
                 .collect(Collectors.toSet());
         return collect;
     }
 
-    protected int heightsDifference (int z_worker, int z_cell)  { //Metodo "protected" perchè mi potrebbe servire in
-        //figlie di questo metodo
+    /**
+     * It's a protected method so that it can be used in classes that
+     * extend this one.
+     *
+     * @param z_worker the position of the worker in the z axis
+     * @param z_cell the position of the cell in the z axis
+     * @returnthe difference in the Z axis between the possible destination Cells and the worker
+     */
+    protected int heightsDifference (int z_worker, int z_cell)  {
         final int i = z_cell - z_worker;
         return i;
     }
